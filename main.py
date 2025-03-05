3a13319cbf814cbcefd7452f850fc90c47276b89
Anthony <blacknebula236@gmail.com>
2025-03-05 19:59:11 +1030

    add Line, Point classes + draw lines in window

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 
 def main():
     win = Window(800, 600)
+    l = Line(Point(50,50), Point(50,400))
+    r = Line(Point(400,50), Point(400,400))
+    t = Line(Point(50,50), Point(400,50))
+    b = Line(Point(50,400), Point(400,400))
+    win.draw_line(l, "red")
+    win.draw_line(r, "blue")
+    win.draw_line(t, "green")
+    win.draw_line(b, "purple")
     win.wait_for_close()
 
 
