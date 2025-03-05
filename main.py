eb8bc66fc9db050f35dd8d6fc2eb931447bb5ab5
Anthony <blacknebula236@gmail.com>
2025-03-05 20:12:26 +1030

    implement cell class + draw cells

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from graphics import Window, Line, Point
+from cell import Cell
 
 
 def main():
     win = Window(800, 600)
-    l = Line(Point(50,50), Point(50,400))
-    r = Line(Point(400,50), Point(400,400))
-    t = Line(Point(50,50), Point(400,50))
-    b = Line(Point(50,400), Point(400,400))
-    win.draw_line(l, "red")
-    win.draw_line(r, "blue")
-    win.draw_line(t, "green")
-    win.draw_line(b, "purple")
+    
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+    
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+    
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
     win.wait_for_close()
 
 
