dfa2a6568de8de405bacc290eaacb39a543c731b
Anthony <blacknebula236@gmail.com>
2025-03-06 23:04:12 +1030

    debug draw_cell

--- maze.py
+++ maze.py
@@ -40,7 +40,7 @@ class Maze:
         if self._win is None:
             return
         x1 = self._x1 + i * self._cell_size_x
-        y1 = self._y1 + i * self._cell_size_y
+        y1 = self._y1 + j * self._cell_size_y
         x2 = x1 + self._cell_size_x
         y2 = y1 + self._cell_size_y
         self._cells[i][j].draw(x1, y1, x2, y2)
